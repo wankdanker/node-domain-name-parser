debd01a30d864ab6af84f0d1d0412b500f094101
Tom Buchok <tom@buchok.com>
2015-02-02 20:06:52 -0500

    test formatting, adds error check

--- test.js
+++ test.js
@@ -16,11 +16,16 @@ test('basic test', function (t) {
 test('country-tlds', function(t) {
   var d = parse('stoner.steve.weedcopter.co.uk');
   t.equal(d.tld, 'co.uk');
-	t.equal(d.sld, 'weedcopter');
-	t.equal(d.domain, 'steve.weedcopter.co.uk');
-	t.equal(d.domainName, 'weedcopter.co.uk');
-	t.equal(d.host, 'stoner');
-	t.equal(d.level(3), 'steve');
-	t.equal(d.level(), 4);
-	t.end();
+  t.equal(d.sld, 'weedcopter');
+  t.equal(d.domain, 'steve.weedcopter.co.uk');
+  t.equal(d.domainName, 'weedcopter.co.uk');
+  t.equal(d.host, 'stoner');
+  t.equal(d.level(3), 'steve');
+  t.equal(d.level(), 4);
+  t.end();
+});
+test('no error on country', function(t) {
+  var d = parse(undefined);
+  t.equal(d.tld, null);
+  t.end(); // it didn't error :)
 });
