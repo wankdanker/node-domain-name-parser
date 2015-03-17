0a7ef063b676b47a0689f772aa4cc7b8c4f6d1f3
Dan VerWeire <dverweire@gmail.com>
2015-03-17 16:19:21 -0400

    Change tabs to two spaces

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var countries = {}
 
 //Create an index of country codes
 lookup.countries.forEach(function (country) {
-	countries[country.internet] = country;
+  countries[country.internet] = country;
 });
 
 module.exports = function (str) {
-	return new DomainName(str);
+  return new DomainName(str);
 };
 
 var COLOMBIAN_DOMAINS = [
@@ -27,43 +27,43 @@ function isCountryMatch(first, second) {
 }
 
 function DomainName(str) {
-	this.tokenized = (str || "").split(/\./gi).reverse();
+  this.tokenized = (str || "").split(/\./gi).reverse();
   var first = (this.tokenized[0] || '').toUpperCase();
   var second = (this.tokenized[1] || '').toUpperCase();
-	if (isCountryMatch(first, second)) {
-		var country = this.tokenized.shift();
-		this.tokenized[0] = [this.tokenized[0], country].join('.');
-	}
+  if (isCountryMatch(first, second)) {
+    var country = this.tokenized.shift();
+    this.tokenized[0] = [this.tokenized[0], country].join('.');
+  }
 }
 
 DomainName.prototype = {
-	get tld() {
-		return this.tokenized[0] || null;
-	}
-	, get sld() {
-		return this.tokenized[1] || null;
-	}
-	, get domainName() {
-		return [this.sld, this.tld].join('.');
-	}
-	, get domain() {
-		var tmp = [].concat(this.tokenized);
-		tmp.pop();
+  get tld() {
+    return this.tokenized[0] || null;
+  }
+  , get sld() {
+    return this.tokenized[1] || null;
+  }
+  , get domainName() {
+    return [this.sld, this.tld].join('.');
+  }
+  , get domain() {
+    var tmp = [].concat(this.tokenized);
+    tmp.pop();
 
-		return tmp.reverse().join('.');
-	}
-	, get host() {
-		return this.tokenized[this.tokenized.length - 1 ];
-	}
-	, level : function (index) {
-		if (!index) {
-			return this.tokenized.length;
-		}
+    return tmp.reverse().join('.');
+  }
+  , get host() {
+    return this.tokenized[this.tokenized.length - 1 ];
+  }
+  , level : function (index) {
+    if (!index) {
+      return this.tokenized.length;
+    }
 
-		return this.tokenized[index - 1] || null;
-	}
-	, toString : function () {
-		return [].concat(this.tokenized).reverse().join('.');
-	}
+    return this.tokenized[index - 1] || null;
+  }
+  , toString : function () {
+    return [].concat(this.tokenized).reverse().join('.');
+  }
 };
 
